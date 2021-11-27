615cb3cf5b2524ed0df3987820b8a30f7599f92f
Sasha <user@gmail.com>
2021-11-27 13:15:23 +0300

    my second commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtCore import QPoint, Qt
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.setFixedSize(self.size())
+        self.pushButton.clicked.connect(self.run)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_circle(qp)
+        qp.end()
+
+    def draw_circle(self, qp):
+        r = randint(10, 200)
+        pen = QPen()
+        pen.setWidth(8)
+        qp.setBrush(Qt.NoBrush)
+        pen.setBrush(QColor(255, 255, 0))
+        qp.setPen(pen)
+        qp.drawEllipse(QPoint(300, 300), r, r)
+
+    def run(self):
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
